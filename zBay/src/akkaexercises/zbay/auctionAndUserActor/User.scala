37ba918053248655336a6740d99f6af028e1b8f6
Howard Perrin <howard@perrin.co.uk>
2013-09-06 10:55:38 +0100

    Add user actor to demonstrate multiple actors/actor refs

---
+++ zBay/src/akkaexercises/zbay/auctionAndUserActor/User.scala
@@ -0,0 +1,21 @@
+package akkaexercises.zbay.auctionAndUserActor
+
+import akka.actor.{ActorRef, Actor}
+
+class User extends Actor {
+  import User.Protocol._
+
+  var auctions = List[ActorRef]()
+
+  def receive = {
+    case BidOnNotification(auction)   => auctions = auction :: auctions
+    case ListAuctionsRequest          => sender ! ListAuctionsResponse(auctions)
+  }
+}
+object User {
+  object Protocol {
+    case object ListAuctionsRequest
+    case class ListAuctionsResponse(auctions: Seq[ActorRef])
+    case class BidOnNotification(auction: ActorRef)
+  }
+}
